fb69f90f09b93a59aef74f2e437c50b4c7aefcde
Bruno Silva <bruno.cassiobdc@gmail.com>
2021-06-27 18:02:54 -0300

    rock paper scisor console game

---
+++ main.js
@@ -0,0 +1,53 @@
+const validPlays = ["rock", "paper", "scisors"];
+
+function computerPlay() {
+    return validPlays[Math.floor(Math.random() * validPlays.length)];
+}
+
+function singleGame(playerSelection, computerselection) {
+    if (validPlays.indexOf(playerSelection.toLowerCase()) < 0) {
+        return `${playerSelection} is a invalid Play`
+    }
+
+    if (playerSelection === computerselection) {
+        return `Draw! you both choose ${playerSelection}`
+    }
+
+    if ((playerSelection === "rock" && computerselection === "scisors") ||
+        (playerSelection === "paper" && computerselection === "rock") ||
+        (playerSelection === "scisors" && computerselection === "Paper")) {
+        return `return you Win! ${playerSelection} beats ${computerselection}`
+    } else {
+        return `return you Loose! ${computerselection} beats ${playerSelection}`
+    }
+
+}
+
+function game() {
+    let singleGameResult = ""
+    let playerScore = 0
+    let computerScore = 0
+ 
+    for (let i = 0; i < 5; i++) {
+        singleGameResult = singleGame(prompt(), computerPlay())
+        
+        if (/Win!/.test(singleGameResult)){
+            playerScore++;
+        }
+        if (/Win!/.test(singleGameResult)){
+            computerScore++;
+        }
+        
+        console.log(`you:${playerScore} cpu:${computerScore}`)
+        console.log(singleGameResult)
+        
+    }
+    
+    if (playerScore > computerScore) {
+        return "You Won! Congratulation";
+    }
+    
+    if (playerScore > computerScore) {
+        return "You Losse! Try Again"
+    }
+}
